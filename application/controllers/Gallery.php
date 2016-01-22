41e7613e7f00755dcbc16a7fc1466617fe87481c
EddyL <kvl192@hotmail.com>
2016-01-22 14:34:11 -0800

    Merge branch 'master' of https://github.com/tseng111/4711-HakunaMatata
    # Conflicts:
    #	application/controllers/Welcome.php
    
    
    ***Basic lab part 3 is done!!!

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -21,6 +21,24 @@ class Gallery extends Application {
 	public function index()
 	{
 		//$this->load->view('gallery');
+            
+            $pix = $this->images->all(); 
+            $cells = [];
+            
+            foreach($pix as $picture) 
+                $cells[] = $this->parser->parse('_cell', (array)$picture, true);
+            
+            $this->load->library('table'); 
+            $parms = array(
+                'table_open' => '<table class="gallery">', 
+                'cell_start' => '<td class="oneimage">', 
+                'cell_alt_start' => '<td class="oneimage">'
+            );
+            $this->table->set_template($parms);
+            
+            $rows = $this->table->make_columns($cells,3); 
+            $this->data['thetable'] = $this->table->generate($rows); 
+            
             $this->data['pagebody'] = 'gallery';
             $this->render();
 	}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,25 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends Application {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		//$this->load->view('welcome');
-                $this->data['pagebody'] = 'welcome';
-                $this->render();
+            
+            $pix = $this->images->newest();
+            $cells = [];
+            
+            foreach($pix as $picture) 
+                $cells[] = $this->parser->parse('_cell', (array) $picture, true); 
+            
+            $this->load->library('table');
+            $parms = array(
+                'table_open' => '<table class="gallery">',
+                'cell_start' => '<td class="oneimage">',
+                'cell_alt_start' => '<td class="oneimage">'
+            );
+            
+            
+            
+            $this->table->set_template($parms);
+  
+            $rows = $this->table->make_columns($cells,3);
+            $this->data['thetable'] = $this->table->generate($rows);
+            
+            $this->data['pagebody'] = 'welcome';
+            $this->render();
 	}
 }
